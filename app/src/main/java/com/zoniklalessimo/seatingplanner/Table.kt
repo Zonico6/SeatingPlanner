b5315bfd84e492917548c0269f9dc438b3e003b1
Zonico6 <Zonico67@gmail.com>
2018-08-16 11:06:36 +0200

    Added methods for handling separators

--- app/src/main/java/com/zoniklalessimo/seatingplanner/Table.kt
+++ app/src/main/java/com/zoniklalessimo/seatingplanner/Table.kt
@@ -28,6 +28,9 @@ fun Table.insertAtStart(other: Table) {
     separators = (other.separators + other.seatCount + separators.map { it + other.seatCount }).toSortedSet()
 }
 
+fun Table.separatorsTillSeat(seat: Int) = separators.filter { it < seat }.toSortedSet()
+fun Table.separatorsFromSeat(seat: Int) = separators.mapNotNull {if (it - seat <= 0) null else it - seat}.toSortedSet()
+
 fun Table.separatorString(concatenate: String): String {
     val builder = StringBuilder((separators.size - 1) * concatenate.length + 1)
     for (i in separators) {
@@ -61,3 +64,48 @@ fun Table.assignSeparators(sepsStr: CharSequence) {
 }
 
 fun Table.addSeparator(pos: Int): Boolean = separators.add(pos)
+fun Table.clearSeparators() = separators.clear()
+
+fun Table.separatorBeforeSeat(seat: Int): Int {
+    var carry = 0
+    for (e in separators) {
+        if (e > seat)
+            return carry
+        carry = e
+    }
+    return carry
+}
+fun Table.separatorAfterSeat(seat: Int): Int {
+    for (e in separators) {
+        if (e > seat)
+            return e
+    }
+    return seatCount
+}
+
+fun Table.sectionAround(seat: Int): Int {
+    var carry = 0
+    for (e in separators) {
+        if (e > seat) {
+            return e - carry
+        }
+        carry = e
+    }
+    return seatCount - carry
+}
+
+fun Table.separatorSpliceAt(seat: Int): Array<SortedSet<Int>> {
+    val firstSeps: SortedSet<Int> = sortedSetOf()
+    val newSeps = sortedSetOf<Int>()
+    var first = true
+    for (i in separators) {
+        if (i > seat && first) {
+            firstSeps.addAll(newSeps)
+            newSeps.clear()
+            first = false
+        } else {
+            newSeps.add(i)
+        }
+    }
+    return arrayOf(firstSeps, newSeps)
+}
